e83b036d306859cc7e341bff840e7b0ce932bc41
MatiasNicolas2013 <112669826+MatiasNicolas2013@users.noreply.github.com>
2023-04-15 01:33:26 +0200

    Añado aqui el error de package
    añado la parte del package

--- Orden.java
+++ Orden.java
@@ -1,14 +1,16 @@
+package ar.com.system2023.mundopc;
 
 public class Orden {
 private final int idOrden;
 private Computadora computadora [];
-private static contadorOrdenes;
+private static int contadorOrdenes = 0;
 private static final int MAX_COMPUTADORAS = 10;
 private int contadorComputadora;
    
 public Orden() {
     this.idOrden = ++Orden.contadorOrdenes;
     this.computadora = new Computadora[Orden.MAX_COMPUTADORAS];
+    this.contadorComputadora = 0;
 }      
             
 

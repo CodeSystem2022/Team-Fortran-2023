e190f790f44bd3e42296dc1a75848133cb6602fb
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-04-15 22:22:38 -0300

    Se creo nueva Clase MundoPc #9
    Se creo la Clase MunpoPc, con su Package, mundoPc.

---
+++ MundoPc/src/mundoPc/MundoPc.java
@@ -0,0 +1,21 @@
+
+package mundoPc;
+
+import ar.com.system2023.mundopc.*;
+
+public class MundoPc {
+    public static void main(String[] args) {
+        //Importar la clase
+        //Objeto 1
+        Monitor monitorHP = new Monitor("HP", 13); 
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Objeto 2
+        Monitor monitorGamer = new Monitor("Gamer", 32); 
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+    }
+}

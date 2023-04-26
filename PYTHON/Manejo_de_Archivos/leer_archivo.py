123d69b4b623517397a274849d4be629351df74d
Fernando <califerdan@gmail.com>
2023-04-26 13:22:10 -0300

    Segunda parte de manejo de archivos

--- PYTHON/Manejo_de_Archivos/leer_archivo.py
+++ PYTHON/Manejo_de_Archivos/leer_archivo.py
@@ -1,5 +1,9 @@
 
-archivo = open('prueba.txt', 'x', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
+archivo = open('prueba.txt', 'r', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
 # print(archivo.read())
+# print(archivo.read(15))
+# print(archivo.read(5)) #continua desde la linea anterior
+print(archivo.readline())
+print(archivo.readline())
 
 

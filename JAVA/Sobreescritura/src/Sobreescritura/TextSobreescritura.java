dcdd4ace2f132eeeaf13b2cca1409522959b0ade
Paola Ruiz <paodelvalle1292@gmail.com>
2023-05-06 07:55:25 -0300

    Se sube la parte de Sobreescritura parte 1 y 2

---
+++ JAVA/Sobreescritura/src/Sobreescritura/TextSobreescritura.java
@@ -0,0 +1,7 @@
+package Sobreescritura;
+public class TextSobreescritura {
+    public static void main(String[]args){
+       Gerente gerente1 = new Gerente("Jose","5000","Sistema"); 
+        System.out.println("gerente =" + gerente1.obtenerDetalles());
+    }
+}

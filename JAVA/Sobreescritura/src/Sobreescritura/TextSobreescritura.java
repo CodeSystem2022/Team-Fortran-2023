feae3e56fbcb37377f88acbd61183e8951f42f6e
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-05-06 10:23:00 -0300

    Polimorfismo Parte 1 y 2 #67
    Se agrego la parte de polimorfismo parte 1 y 2. También, el proyecto se paso a una carpeta, para tenerlo mas ordenado.

--- JAVA/Sobreescritura/src/Sobreescritura/TextSobreescritura.java
+++
@@ -1,7 +0,0 @@
-package Sobreescritura;
-public class TextSobreescritura {
-    public static void main(String[]args){
-       Gerente gerente1 = new Gerente("Jose","5000","Sistema"); 
-        System.out.println("gerente =" + gerente1.obtenerDetalles());
-    }
-}

bb6459d8f5acd0ae8082d86c7f10331c27d1b15b
Paola Ruiz <paodelvalle1292@gmail.com>
2023-05-06 09:43:42 -0300

    Se saca un espacio

--- JAVA/Sobreescritura/src/domain/Gerente.java
+++ JAVA/Sobreescritura/src/domain/Gerente.java
@@ -2,7 +2,7 @@ package domain;
 
 public class Gerente extends Empleado{
     private String departamento;
-    public Gerente(String nombre, double sueldo,String departamento){
+    public Gerente(String nombre,double sueldo,String departamento){
         super(nombre,sueldo);
         this.departamento = departamento;
     }

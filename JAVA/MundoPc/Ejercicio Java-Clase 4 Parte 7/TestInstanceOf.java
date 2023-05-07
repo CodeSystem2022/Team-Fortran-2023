257b16b5a47df00c395f2b34f35c9c64ab55184d
MatiasNicolas2013 <112669826+MatiasNicolas2013@users.noreply.github.com>
2023-05-08 00:41:05 +0200

    Ejercicio con instanceof 1y2
    A continuacion,entrego los ejercicios tanto 1 y 2

---
+++ JAVA/MundoPc/Ejercicio Java-Clase 4 Parte 7/TestInstanceOf.java
@@ -0,0 +1,37 @@
+
+package test;
+
+import domain.*;
+
+public class TestInstanceOf {
+    
+    public static void main(String[]args){
+        
+        Empleado empleado1 = new Empleado("Juan", 10000);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+        //determinarTipo(empleado1);
+
+    }
+    
+    public static void determinarTipo(Empleado empleado){
+        if(empleado instanceof Gerente){
+            System.out.println("Es de tipo Gerente");
+            //MatiasPeña-Ejercicios con instanceof 1 y2
+           //((Gerente)empleado).getDepartamento();
+           Gerente gerente = (Gerente)empleado;
+           System.out.println("gerente = "+gerente.getDepartamento);
+           //gerente.getDepartamento();
+        }
+        else if(empleado instanceof Empleado){
+            System.out.println("Es de tipo Empleado");
+           //Gerente gerente = (Gerente)empleado;
+           //System.out.println("gerente ="+gerente.getDepartamento());
+        }
+        else if(empleado instanceof Object){
+            System.out.println("Es de tipo Object");
+        }
+            
+        }
+    }
+

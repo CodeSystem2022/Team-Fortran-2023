5426bdbc7d4f1f5b38ade601fda9d945f67dbb19
MatiasNicolas2013 <112669826+MatiasNicolas2013@users.noreply.github.com>
2023-05-29 01:24:07 +0200

    05-03-clases.js
    Añado lo que falta al archivo main

--- JavaScript/Semana8/05-03-clases.js
+++ JavaScript/Semana8/05-03-clases.js
@@ -7,7 +7,27 @@ class Persona{
     get nombre(){
         return this._nombre;
     }
+      //Clase 6 -Matias Peña-Get y Setter parte 2
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this.apellido = apellido;
+    }
+
 }
+let persona1 = new Persona('Martin', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+//console.log(persona1);
+let persona2 = new Persona('Carlos','Lara');
+persona2.nombre = 'Maria Laura';
+console.log(persona2.nombre);
+console.log(persona2.apellido)
+//console.log(persona2);
+
 //Herencia parte 1
 class Empleado extends Persona{// clase hija
     constructor(departamento){

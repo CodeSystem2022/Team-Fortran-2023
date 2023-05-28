b058e272b584cd3d1b228ff9bc579d3b076c151a
Gisela <giselavizcaino5@gmail.com>
2023-05-28 20:51:18 -0300

    1.4 Hoisting y clases: Parte 2 #126
    Agregamos Hoisting y clases: Parte 2

--- JavaScript/Semana8/05-03-clases.js
+++ JavaScript/Semana8/05-03-clases.js
@@ -1,4 +1,5 @@
-let persona3 = new Persona('Carla', 'Ponce');
+//let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer : persona is not defined 
+
 class Persona{
     constructor(nombre, apellido){
         this.nombre = nombre;
@@ -7,6 +8,18 @@ class Persona{
     
     get nombre(){
         return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
     }
 }
 //Herencia parte 1

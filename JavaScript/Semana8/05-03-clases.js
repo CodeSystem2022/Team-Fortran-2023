44661eaad0fae2a760718124f413297a5d0cf084
Matias <nimats2018@gmail.com>
2023-05-29 20:31:09 +0200

    repaso 4

--- JavaScript/Semana8/05-03-clases.js
+++ JavaScript/Semana8/05-03-clases.js
@@ -13,7 +13,7 @@ class Persona{
     set nombre(nombre){
         this._nombre = nombre;
     }
-//Clase 6- Get y Setter-parte 2-Matias Pe?a
+//Clase 6- Get y Setter-parte 2-Matias Peña
     get apellido(){
         return this._apellido;
     }

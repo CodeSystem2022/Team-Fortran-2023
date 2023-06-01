e2a4d7f9cfd58829941e2be7b54cfeb3dfbdaa2b
Facundo Pereyra <facundo070598@gmail.com>
2023-05-31 21:03:12 -0300

    Prueba de la clase persona #142
    Prueba de la clase persona

--- PYTHON/Persona.py
+++ PYTHON/Persona.py
@@ -1,5 +1,7 @@
+from logger_base import log
+
 class Persona:
-    def __init__(self, id_persona, nombre, apellido, email, ):
+    def __init__(self, id_persona=None, nombre=None, apellido=None, email=None):
         self._id_persona = id_persona
         self.nombre = nombre
         self.apellido = apellido
@@ -41,4 +43,10 @@ def email(self):
 def email (self, email):
     self.email = email
 
-
+if __name__ == '__main__':
+    persona1 = Persona(1, 'Juan', 'Perez', 'jperez@gmail.com')
+    log.debug(persona1)
+    persona2 = Persona(nombre='Jose', apellido='Lepez', email='ljose@gmail.com')
+    log.debug(persona2)
+    persona1 = Persona(id_persona = 1)
+    log.debug(persona1)

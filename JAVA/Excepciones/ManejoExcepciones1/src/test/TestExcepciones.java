9b1e5d4cf4d2fcec43765f6fc81f99579c4b2d12
danisa-morales <64617071+danisa-morales@users.noreply.github.com>
2023-06-09 15:33:14 -0300

    Comenzamos con Excepciones: Parte 4 #163 (#173)

--- JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
+++ JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -3,15 +3,13 @@ package test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        //int resultado = 10/0;
-        //System.out.println("resultado = " + resultado);
         int resultado = 0;
-        try {
+        //try {
             resultado = 10/0;
-        }catch(Exception e){
-            System.out.println("Ocurrio un Error");
-            e.printStackTrace(System.out); //se conoce como la pila de excepciones
-        }
+//        }catch(Exception e){
+//            System.out.println("Ocurrio un Error");
+//            e.printStackTrace(System.out); //se conoce como la pila de excepciones
+//        }
         System.out.println("La variable de resultado tiene como valor: "+resultado);
     }
 }

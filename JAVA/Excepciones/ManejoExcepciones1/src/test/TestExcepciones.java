16ddb3796ddad3e02853667374a6c41de1c044f1
MatiasNicolas2013 <112669826+MatiasNicolas2013@users.noreply.github.com>
2023-06-12 00:39:50 +0200

    Guardando cambios (#175)

--- JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
+++ JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -4,12 +4,14 @@ package test;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-        try {
+        //try {
             resultado = 10/0;
-        }catch(Exception e){
-            System.out.println("Ocurrio un Error");
-            e.printStackTrace(System.out); //se conoce como la pila de excepciones
-        }
+//        }catch(Exception e){
+//            System.out.println("Ocurrio un Error");
+//            e.printStackTrace(System.out); //se conoce como la pila de excepciones
+//        }
         System.out.println("La variable de resultado tiene como valor: "+resultado);
     }
 }
+
+

7095eabd90eae9af27b8fac89b50b749303b7360
MatiasNicolas2013 <nimats2018@gmail.com>
2023-06-11 23:24:23 +0200

    Guardando cambios

--- JAVA/Excepciones/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
+++ JAVA/Excepciones/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
@@ -1,9 +1,9 @@
-
 package excepciones;
 
-public class OperacionExcepcion extends Exception{
-    //Constructor  
-    public OperacionExcepcion(String mensaje){
+public class OperacionExcepcion extends Exception {
+    // Constructor  
+    public OperacionExcepcion(String mensaje) {
         super(mensaje);
     }
 }
+

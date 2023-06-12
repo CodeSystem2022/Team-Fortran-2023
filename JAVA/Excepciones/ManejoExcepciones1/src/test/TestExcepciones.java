d2a25770b586f4613a60c331e399e8566fac3371
Morales1227 <100379618+Morales1227@users.noreply.github.com>
2023-06-11 21:45:27 -0300

    Comenzamos a hacer pruebas con RuntimeException #(167) (#177)
    Pruebas de RuntimeException

--- JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
+++ JAVA/Excepciones/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -6,16 +6,8 @@ import static aritmetica.Aritmetica.division;
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-
-        try {
-            resultado = division(10,0);
-        }catch(Exception e){
-            System.out.println("Ocurrio un Error");
-            e.printStackTrace(System.out); //se conoce como la pila de excepciones
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("La variable de resultado tiene como valor: "+resultado);
+        resultado = division(10, 0);
+        System.out.println("La variable de resultado tiene como valor: " + resultado);
     }
 }
 

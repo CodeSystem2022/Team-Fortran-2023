64ece10a93a2de46962f83aa96e7bb6695ede261
danisa-morales <moralesc.danisa@gmail.com>
2023-06-21 19:54:48 -0300

    Creamos la Clase Orden: Parte 1 #181

--- JavaScript/Semana11/SistemaVentas.js
+++ JavaScript/Semana11/SistemaVentas.js
@@ -31,3 +31,36 @@ class Producto{
 
     }
 }//Fin de la clase Producto
+
+class Orden{
+    static contadorOrdenes = 0;
+    static getMAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.length < Orden.getMAX_PRODUCTOS()){
+            this._productos.push(producto); // Tenemos 2 tipos de sintaxis: 1
+            //this._productos[this._contadorProductosAgregados++] = producto; // segunda sintaxis
+        }
+        else{
+            console.log('No se puede agregar más productos');
+        }
+    } // Fin del método agregarProducto
+} // Fin de la Clase Orden
+
+
+let producto1 = new Producto('Pantalón', 200);
+let producto2 = new Producto('Camisa', 150);
+console.log(producto1.toString());
+console.log(producto2.toString());

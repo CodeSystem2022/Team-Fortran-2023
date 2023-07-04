a44c5c0e1a44da936cd015face6294373a0a0b91
MatiasNicolas2013 <nimats2018@gmail.com>
2023-07-05 00:11:14 +0200

    Clase 6-parte 5

--- PYTHON/Clase 6-Python/transacciones.py
+++ PYTHON/Clase 6-Python/transacciones.py
@@ -2,19 +2,20 @@ import psycopg2 as bd
 
 # Esto es para poder conectarnos a Postgres
 
-conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+conexion = bd.connect(user='postgres', password='admin', host='localhost', port='8090', database='test_bd')
 try:
-    conexion.autocommit = False
+    # conexion.autocommit = False #Esto no deberia estar
     cursor = conexion.cursor()
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
     valores = ('Maria', 'Esparza', 'mersparza@gmail.com')
     cursor.execute(sentencia, valores)
-    conexion.commit()  # Guardar los cambios en la base de datos
+    conexion.commit()  # Hacemos el commit manualmente
     print('Termina la transacción')
 
 except Exception as e:
-    print(f'Ocurrió un error: {e}')
     conexion.rollback()  # Revertir los cambios en caso de error
+    print(f'Ocurrió un error: {e}')
+
 
 finally:
     if conexion is not None:
